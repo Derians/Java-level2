b35d0406376ac89115e40bebf86b069885c3ba81
Ivan Chaykin <deriansv@gmail.com>
2018-12-25 15:23:43 +0300

    Add ChatWindow class

---
+++ Lesson4/src/Lesson4.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Ivan Chaykin
+ * Date: 25.12.2018
+ * Time: 10:39
+ */
+public class Lesson4 {
+
+    public static void main(String[] args) {
+        new ChatWindow();
+    }
+
+}
