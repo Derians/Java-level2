113621628519bd6a46ab2eb1a4bc654df971b323
Derians <deriansv@gmail.com>
2019-01-16 23:00:02 +0300

    Add Lesson6 class Add Client class Add Server class

---
+++ Lesson6/src/Client.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Ivan Chaykin
+ * Date: 15.01.2019
+ * Time: 23:04
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        new Lesson6();
+    }
+
+}
---
+++ Lesson6/src/Server.java
@@ -0,0 +1,71 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * Created by Ivan Chaykin
+ * Date: 15.01.2019
+ * Time: 22:18
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket client = null;
+        try {
+            serverSocket = new ServerSocket(8080);
+            System.out.println("Сервер ожидает подключения");
+            client = serverSocket.accept();
+            System.out.println("Клиент подключен");
+            Scanner scanner = new Scanner(client.getInputStream());
+            PrintWriter printWriter = new PrintWriter(client.getOutputStream());
+
+            Thread readerThread = new Thread(new ServerMessageHandler(printWriter));
+            readerThread.start();
+
+            while (true) {
+                String message = scanner.nextLine();
+                if ("end".equalsIgnoreCase(message)) {
+                    break;
+                }
+                printWriter.println("Client: " + message);
+                System.out.println("Client: " + message);
+                printWriter.flush();
+            }
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } finally {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class ServerMessageHandler implements Runnable {
+
+        PrintWriter printWriter;
+
+        public ServerMessageHandler(PrintWriter printWriter) {
+            this.printWriter = printWriter;
+        }
+        Scanner sc = new Scanner(System.in);
+        public void run() {
+            String serverMessage;
+            try {
+                while (true) {
+                    serverMessage = sc.nextLine();
+                    if ("end".equalsIgnoreCase(serverMessage)) {
+                        break;
+                    }
+                    printWriter.println("Server: " + serverMessage);
+                    printWriter.flush();
+                }
+            } catch(Exception e) { e.printStackTrace(); }
+        }
+    }
+
+}
